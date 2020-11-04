38a4fb3de5856ec235bf8c7fec92848039b4eeef
Robert Clarke <r.jason.clarke@gmail.com>
2020-11-04 00:42:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,10 @@ def roll_call_dwarves(array)
   array.each_with_index {|name, index| puts "#{index + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  planeteer_calls = []
+  array.collect  do |calls|
+    planeteer_calls << "#{calls.capitalize}!"
 end
 
 def long_planeteer_calls# code an argument here
